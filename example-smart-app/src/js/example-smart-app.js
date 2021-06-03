66db4a5e260e972a52e898f38733859b2ba42ac3
pkolasimperial <prasad.kolas@nhs.net>
2021-06-03 13:27:47 +0100

    Update example-smart-app.js
    Removed window

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -1,5 +1,5 @@
-(function(window){
-  window.extractData = function() {
+
+var extractData = function() {
     var ret = $.Deferred();
 
     function onError() {
@@ -70,62 +70,62 @@
     FHIR.oauth2.ready(onReady, onError);
     return ret.promise();
 
-  };
+};
 
-  function defaultPatient(){
-    return {
-      fname: {value: ''},
-      lname: {value: ''},
-      gender: {value: ''},
-      birthdate: {value: ''},
-      height: {value: ''},
-      systolicbp: {value: ''},
-      diastolicbp: {value: ''},
-      ldl: {value: ''},
-      hdl: {value: ''},
-    };
+function defaultPatient(){
+return {
+  fname: {value: ''},
+  lname: {value: ''},
+  gender: {value: ''},
+  birthdate: {value: ''},
+  height: {value: ''},
+  systolicbp: {value: ''},
+  diastolicbp: {value: ''},
+  ldl: {value: ''},
+  hdl: {value: ''},
+};
+}
+
+function getBloodPressureValue(BPObservations, typeOfPressure) {
+var formattedBPObservations = [];
+BPObservations.forEach(function(observation){
+  var BP = observation.component.find(function(component){
+	return component.code.coding.find(function(coding) {
+	  return coding.code == typeOfPressure;
+	});
+  });
+  if (BP) {
+	observation.valueQuantity = BP.valueQuantity;
+	formattedBPObservations.push(observation);
   }
+});
 
-  function getBloodPressureValue(BPObservations, typeOfPressure) {
-    var formattedBPObservations = [];
-    BPObservations.forEach(function(observation){
-      var BP = observation.component.find(function(component){
-        return component.code.coding.find(function(coding) {
-          return coding.code == typeOfPressure;
-        });
-      });
-      if (BP) {
-        observation.valueQuantity = BP.valueQuantity;
-        formattedBPObservations.push(observation);
-      }
-    });
+return getQuantityValueAndUnit(formattedBPObservations[0]);
+}
 
-    return getQuantityValueAndUnit(formattedBPObservations[0]);
-  }
+function getQuantityValueAndUnit(ob) {
+if (typeof ob != 'undefined' &&
+	typeof ob.valueQuantity != 'undefined' &&
+	typeof ob.valueQuantity.value != 'undefined' &&
+	typeof ob.valueQuantity.unit != 'undefined') {
+	  return ob.valueQuantity.value + ' ' + ob.valueQuantity.unit;
+} else {
+  return undefined;
+}
+}
 
-  function getQuantityValueAndUnit(ob) {
-    if (typeof ob != 'undefined' &&
-        typeof ob.valueQuantity != 'undefined' &&
-        typeof ob.valueQuantity.value != 'undefined' &&
-        typeof ob.valueQuantity.unit != 'undefined') {
-          return ob.valueQuantity.value + ' ' + ob.valueQuantity.unit;
-    } else {
-      return undefined;
-    }
-  }
+var drawVisualization = function(p) {
+$('#holder').show();
+$('#loading').hide();
+$('#fname').html(p.fname);
+$('#lname').html(p.lname);
+$('#gender').html(p.gender);
+$('#birthdate').html(p.birthdate);
+$('#height').html(p.height);
+$('#systolicbp').html(p.systolicbp);
+$('#diastolicbp').html(p.diastolicbp);
+$('#ldl').html(p.ldl);
+$('#hdl').html(p.hdl);
+};
 
-  window.drawVisualization = function(p) {
-    $('#holder').show();
-    $('#loading').hide();
-    $('#fname').html(p.fname);
-    $('#lname').html(p.lname);
-    $('#gender').html(p.gender);
-    $('#birthdate').html(p.birthdate);
-    $('#height').html(p.height);
-    $('#systolicbp').html(p.systolicbp);
-    $('#diastolicbp').html(p.diastolicbp);
-    $('#ldl').html(p.ldl);
-    $('#hdl').html(p.hdl);
-  };
 
-})(window);
