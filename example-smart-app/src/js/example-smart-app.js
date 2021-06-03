14fc9c05e9675daa730560616dc6b97d18ed9ab4
pkolasimperial <prasad.kolas@nhs.net>
2021-06-03 14:09:12 +0100

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -19,7 +19,7 @@ var extractData = function() {
                               'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
                               'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
                       },
-					  date: 'gt2021-01-01',
+					  date: 'gt2019-01-01',
 					  category: 'vital-signs'
                     }
                   });
