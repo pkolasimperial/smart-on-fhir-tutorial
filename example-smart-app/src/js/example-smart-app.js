98d3cc2bbb19f1784fff6139bcb28c141190ba9e
pkolasimperial <prasad.kolas@nhs.net>
2021-06-03 14:46:05 +0100

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -33,11 +33,12 @@ var extractData = function() {
                     }
                   });
 
-        $.when(pt, obv).fail(onError);
+        $.when(pt, obv, alg).fail(onError);
 
-        $.when(pt, obv).done(function(patient, obv) {
+        $.when(pt, obv, alg).done(function(patient, obv, allergies) {
 		  console.log(patient);
 		  console.log(obv);
+		  console.log(allergies);
           var byCodes = smart.byCodes(obv, 'code');
           var gender = patient.gender;
 
